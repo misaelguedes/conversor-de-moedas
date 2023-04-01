59cff308f1eaa9ab05a44ebac7b92d07ab090af2
misaelguedes <124177593+misaelguedes@users.noreply.github.com>
2023-04-01 16:39:00 -0300

    Atualizei valores do dólar, do euro e da libra

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let vdolar = 5.24
-let veuro = 5.70
-let vlibra = 6.44
+let vdolar = 5.06
+let veuro = 5.50
+let vlibra = 6.25
 
 function RealemDolar() {
     let realdolar = document.getElementById('irealemdolar')
